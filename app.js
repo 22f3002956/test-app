976d7f4fcf8da8b2c45c259d997a6c0fc49ccace
22f3002956 <22f3002956@ds.study.iitm.ac.in>
2025-10-16 19:26:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,2 @@
-// No JavaScript needed for this simple app.
+// JavaScript functionality can be added here if needed.
+// For now, this file is empty as the requirements do not specify any interactivity.
